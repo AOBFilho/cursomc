63310f1348f5fcca1f6e25ad65629a01677cb038
Antonio Brandão <antonioobfilho@gmail.com>
2018-05-09 15:00:13 -0300

    Atualizando anotacoes do jackson

--- src/main/java/com/aobfilho/cursomc/domain/Cidade.java
+++ src/main/java/com/aobfilho/cursomc/domain/Cidade.java
@@ -9,10 +9,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
-@JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
 @Entity
 public class Cidade implements Serializable{
 
