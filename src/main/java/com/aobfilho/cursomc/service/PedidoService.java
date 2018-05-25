01c71d86ca225f923842d4ceb70cb3a49c8846fe
Antonio Brandão <antonioobfilho@gmail.com>
2018-05-25 11:58:34 -0300

    Restricao de acesso: Cliente so recupera seus pedidos

--- src/main/java/com/aobfilho/cursomc/service/PedidoService.java
+++ src/main/java/com/aobfilho/cursomc/service/PedidoService.java
@@ -5,9 +5,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.aobfilho.cursomc.domain.Cliente;
 import com.aobfilho.cursomc.domain.ItemPedido;
 import com.aobfilho.cursomc.domain.PagamentoComBoleto;
 import com.aobfilho.cursomc.domain.Pedido;
@@ -15,7 +19,9 @@ import com.aobfilho.cursomc.domain.enums.EstadoPagamento;
 import com.aobfilho.cursomc.repositories.ItemPedidoRepository;
 import com.aobfilho.cursomc.repositories.PagamentoRepository;
 import com.aobfilho.cursomc.repositories.PedidoRepository;
+import com.aobfilho.cursomc.security.UserSpringSecurity;
 import com.aobfilho.cursomc.service.email.EmailService;
+import com.aobfilho.cursomc.service.exceptions.AuthorizationException;
 import com.aobfilho.cursomc.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -75,5 +81,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSpringSecurity user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
 	}
 }
