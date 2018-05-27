9b7b9ef7dd57e64e892db184dcbbddc275055dc7
Antonio Brandão <antonioobfilho@gmail.com>
2018-05-26 21:55:56 -0300

    Esqueci a senha

--- src/main/java/com/aobfilho/cursomc/resources/AuthResource.java
+++ src/main/java/com/aobfilho/cursomc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.aobfilho.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aobfilho.cursomc.dto.EmailDTO;
 import com.aobfilho.cursomc.security.JWTUtil;
 import com.aobfilho.cursomc.security.UserSpringSecurity;
+import com.aobfilho.cursomc.service.AuthService;
 import com.aobfilho.cursomc.service.UserService;
 
 @RestController
@@ -19,11 +23,20 @@ public class AuthResource {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
+	@Autowired
+	private AuthService authService;
+	
 	@PostMapping(value="/refresh_token")
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response){
 		UserSpringSecurity user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer "+token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PostMapping(value="/forgot")
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDTO){
+		authService.sendNewPassword(objDTO.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 
--- src/main/java/com/aobfilho/cursomc/service/email/EmailService.java
+++ src/main/java/com/aobfilho/cursomc/service/email/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.aobfilho.cursomc.domain.Cliente;
 import com.aobfilho.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -13,5 +14,6 @@ public interface EmailService {
 	
 	void sendEmail(SimpleMailMessage msg);
 	void sendHtmlEmail(MimeMessage msg);
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 	
 }
